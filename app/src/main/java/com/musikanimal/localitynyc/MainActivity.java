9403b10ed6f03300d4ace7e28761669f2877f8f7
MusikAnimal <musikanimal@gmail.com>
2019-05-29 21:06:31 -0400

    Initial working version

---
+++ app/src/main/java/com/musikanimal/localitynyc/MainActivity.java
@@ -0,0 +1,35 @@
+package com.musikanimal.localitynyc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        mWebView = findViewById(R.id.activity_main_webview);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setGeolocationEnabled(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setDomStorageEnabled(true);
+
+        mWebView.clearCache(true);
+
+        mWebView.setWebViewClient(new WebViewClient());
+
+        mWebView.loadUrl("https://locality.nyc");
+    }
+}
