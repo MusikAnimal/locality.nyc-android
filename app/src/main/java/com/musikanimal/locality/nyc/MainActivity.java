16eb0735fa2fc1cabb3130585135081e38ebe515
MusikAnimal <musikanimal@gmail.com>
2019-06-03 22:23:42 -0400

    Require geolocation permission at runtime

--- app/src/main/java/com/musikanimal/locality/nyc/MainActivity.java
+++ app/src/main/java/com/musikanimal/locality/nyc/MainActivity.java
@@ -1,10 +1,11 @@
 package com.musikanimal.locality.nyc;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
         mWebView = findViewById(R.id.activity_main_webview);
 
@@ -28,7 +31,11 @@ public class MainActivity extends AppCompatActivity {
 
         mWebView.clearCache(true);
 
-        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebChromeClient(new android.webkit.WebChromeClient() {
+            public void onGeolocationPermissionsShowPrompt(String origin, android.webkit.GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
 
         mWebView.loadUrl("https://locality.nyc");
     }
